ef089643ae470f19f435386a27c4c0f78eab798a
YuviPanda <yuvipanda@gmail.com>
2013-06-01 19:38:56 +0530

    All the code!
    Sortof 'working' version (hardcoded to my name), that displays things
    on the widget. Needs sexiness.
    
    Also uses Volley

---
+++ editcountitis/src/main/java/in/yuvi/editcountitis/widget/EditsWidgetProvider.java
@@ -0,0 +1,45 @@
+package in.yuvi.editcountitis.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.Override;
+import java.util.concurrent.CountDownLatch;
+
+import in.yuvi.editcountitis.R;
+
+public class EditsWidgetProvider extends AppWidgetProvider {
+    public static final String TAG = "Editcountitis";
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context,
+                EditsWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+        Log.d(TAG, "Hello?");
+
+        // Build the intent to call the service
+        Intent intent = new Intent(context.getApplicationContext(),
+                EditsWidgetUpdaterService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+
+        // Update the widgets via the service
+        context.startService(intent);
+        Log.d(TAG, "Updated, yo!");
+    }
+}
---
+++ editcountitis/src/main/java/in/yuvi/editcountitis/widget/EditsWidgetUpdaterService.java
@@ -0,0 +1,79 @@
+package in.yuvi.editcountitis.widget;
+
+import java.net.HttpURLConnection;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.http.AndroidHttpClient;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import in.yuvi.editcountitis.R;
+
+public class EditsWidgetUpdaterService extends Service {
+    private static final String TAG = "Editcountitis";
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Log.d(TAG, "Called");
+        // Create some random data
+
+        String url = "https://en.wikipedia.org/w/api.php?action=query&list=users&format=json&usprop=editcount&ususers=Yuvipanda";
+        final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
+                .getApplicationContext());
+
+        int[] allWidgetIds = intent
+                .getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+
+        ComponentName thisWidget = new ComponentName(getApplicationContext(),EditsWidgetProvider.class);
+       
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        for (final int widgetId : allWidgetIds) {
+            // Create some random data
+            int number = (new Random().nextInt(100));
+
+            final RemoteViews remoteViews = new RemoteViews(this
+                    .getApplicationContext().getPackageName(),
+                    R.layout.widget_layout);
+
+            queue.add(new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject result) {
+                    String count = result.optJSONObject("query").optJSONArray("users").optJSONObject(0).optString("editcount");
+                    remoteViews.setTextViewText(R.id.count_alledits, count);
+                    appWidgetManager.updateAppWidget(widgetId, remoteViews);
+                    Log.d(TAG, "DONE!~");
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    Log.d(TAG, "AW FUCK");
+                }
+            }
+            ));
+        }
+
+        queue.start();
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
